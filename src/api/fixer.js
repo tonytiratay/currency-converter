e85733a08cf1dda3e8b75db19438a716c7e364fc
Tony Tiratay <tony.tiratay@gmail.com>
2018-11-01 03:29:16 +0100

    fix recursive error if no network

--- src/api/fixer.js
+++ src/api/fixer.js
@@ -7,7 +7,6 @@ import axios from 'axios';
 import { API_KEY } from '../config';
 
 // Récupérer la liste de toutes les devises
-console.log('API_KEY: ',API_KEY);
 export const fetchDevises = () => {
 
 	// Preparer l'url pour la requête
@@ -19,7 +18,6 @@ export const fetchDevises = () => {
 			// Stocker dans "devises" les résultats fournis par l'API
 
 			const devises = results.data.symbols;
-			console.log('devises: ',devises);
 			// Préparer les devise dans un tableau au format attendu
 			// par le dropdown de semantic-ui
 
@@ -33,7 +31,6 @@ export const fetchDevises = () => {
 			   });
 			});
 
-			console.log('array: ',array);
 			return array;
 		})
 		.catch((error) => {
@@ -56,13 +53,11 @@ export const getConversionRates = () => {
 		.then((conversion) => {
 
 			// Extraire et renvoyer les ratios de conversion
-			console.log('conversion: ',conversion.data.rates);
 			return conversion.data.rates;
 		})
 		.catch((error) => {
 			
 			// En cas d'erreur, renvoyer un objet avec une propriété error
-			console.log('error: ',error);
 			return {error}
 		});
 	return result
